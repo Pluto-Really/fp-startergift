d9a12569a1492f69c01d68418f8bf7bf587d5e65
Fuckin-Pluto <92813476+Pluto-Really@users.noreply.github.com>
2022-06-10 14:58:17 -0400

    Update local QBCore = exports['qb-core']:GetCoreObject()

--- server/sv_gift.lua
+++ server/sv_gift.lua
@@ -1,3 +1,5 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
 QBCore.Functions.CreateUseableItem("starter_gift", function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player.Functions.RemoveItem(item.name, 1, item.slot) then
